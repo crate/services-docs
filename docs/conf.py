b304bdbb9bb445298428410acdb2b9ddde90b8ec
Andreas Motl <andreas.motl@crate.io>
2023-08-04 04:29:18 +0200

    Disable version chooser. This project is not versioned.

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,12 @@
 from crate.theme.rtd.conf.cloud import *
 
+# Disable version chooser.
+html_context.update({
+    "display_version": False,
+    "current_version": None,
+    "versions": [],
+})
+
 linkcheck_ignore = [
     "https://eks1.eu-west-1.aws.cratedb.cloud",
     "https://eastus2.azure.cratedb.cloud/",
