c2f2465892975d97e465a9173b783fd4fa770dc1
Andreas Motl <andreas.motl@crate.io>
2024-03-14 22:08:21 +0100

    Chore: Ignore linkcheck errors on https://azure.microsoft.com
    HTTPSConnectionPool(host='azure.microsoft.com', port=443):
    Read timed out. (read timeout=5)

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ linkcheck_ignore = [
     "https://eastus2.azure.cratedb.cloud/",
     "https://portal.azure.com/",
     "https://azuremarketplace.microsoft.com/",
+    "https://azure.microsoft.com/",
 ]
 
 linkcheck_timeout = 5
